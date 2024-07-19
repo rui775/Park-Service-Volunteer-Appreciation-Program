bbbb5a778cfe1bec4d5c714aa67c329d68c90865
rui775 <rsilva6420.dev@gmail.com>
2024-07-19 09:53:17 +0100

    Complete combineVolunteers function

---
+++ index.ts
@@ -0,0 +1,70 @@
+import {
+    RaccoonMeadowsVolunteers,
+    RaccoonMeadowsActivity,
+    raccoonMeadowsVolunteers,
+} from './raccoon-meadows-log';
+
+import {
+    WolfPointVolunteers,
+    WolfPointActivity,
+    wolfPointVolunteers,
+} from './wolf-point-log';
+
+type CombinedActivity = RaccoonMeadowsActivity | WolfPointActivity;
+
+type Volunteers = {
+    id: number;
+    name: string;
+    activities: CombinedActivity[];
+};
+
+function combineVolunteers(
+    volunteers: (RaccoonMeadowsVolunteers | WolfPointVolunteers)[]
+) {
+    return volunteers.map((volunteer) => {
+        let id = volunteer.id;
+
+        if (typeof id === 'string') {
+            id = parseInt(id, 10);
+        }
+
+        return {
+            id: id,
+            name: volunteer.name,
+            activities: volunteer.activities,
+        }
+    })
+}
+
+function isVeridied(verified: string | boolean) {
+    if (typeof verified === 'string') {
+        if (verified === 'Yes') {
+            return true;
+        }
+        if (verified === 'No') {
+            return false;
+        }
+    }
+    return verified;
+}
+function calculateHours(volunteers: Volunteers[]) {
+    return volunteers.map((volunteer) => {
+        let hours = 0;
+
+        volunteer.activities.forEach((activity) => {
+
+        });
+
+        return {
+            id: volunteer.id,
+            name: volunteer.name,
+            hours: hours,
+        };
+    });
+}
+
+const combinedVolunteers = combineVolunteers(
+    [].concat(wolfPointVolunteers, raccoonMeadowsVolunteers)
+);
+
+console.log(JSON.stringify(combinedVolunteers, null, 2))
